c1f80d1c188a1e6797c0c4c01c67053d204759ae
Apoorv Saini <apoorvsaini9@gmail.com>
2018-03-13 14:25:30 -0400

    Initial commit

---
+++ src/start/index.js
@@ -0,0 +1,12 @@
+export const greet = () => {
+    return "Hello World!";
+  };
+  
+  export const sidebar = () => {
+    return '<div id="sidebar"><div id="header">SideLine</div></div>';
+  };
+
+  export const bye = () => {
+    return "See ya!";
+  };
+  
