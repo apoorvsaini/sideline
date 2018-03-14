9102b6f3aa1b94ecb772064424ea393cd6c0a772
Apoorv Saini <apoorvsaini9@gmail.com>
2018-03-13 23:37:12 -0400

    Fixes in score line

--- src/start/index.js
+++ src/start/index.js
@@ -13,18 +13,38 @@ const store = new Store({
     draws: 0
   }
 });
+
+const currMatch = new Store({
+  configName: 'match-data',
+  defaults: {
+    opp_name: "Opponent",
+    self_venue: "home",
+    self_score: 0,
+    opp_score: 0
+  }
+});
+
 var name = store.get('name');
+var opp_name = currMatch.get('opp_name');
+var venue = currMatch.get('self_venue');
 console.log(store);
 
 var loginDom = '<div id="profile_area"><input id="name_input" placeholder="Your Name"/><button id="login_btn" >Save Profile</button></div>';
 
+var userScore = '<div id="profile_saved_area"> W: '+store.get('wins')+' L: '+store.get('losses')+' D: '+store.get('draws')+' </div>';
+
 if (name != '') loginDom = '<div id="profile_saved_area"> Welcome '+name+'!</div>';
 
-var scoreCardDom = '<div id="score_card"> <span class="team_name left_right_margin">'+matchDetails.awayTeam+'</span> '+matchDetails.homeScore+' : '+matchDetails.awayScore+' <span class="team_name left_right_margin">'+matchDetails.homeTeam+'</span></div> <div id="field_area"></div>';
+var scoreCardDom = '<div id="score_card"> <span class="team_name left_right_margin">'+name+' FC</span> '+currMatch.get('self_score')+' : '+currMatch.get('opp_score')+' <span class="team_name left_right_margin">'+currMatch.get('opp_name')+' FC</span></div> <div id="field_area"></div>';
+
+if(venue == 'away') {
+  scoreCardDom = '<div id="score_card"> <span class="team_name left_right_margin">'+matchDetails.awayTeam+'</span> '+matchDetails.homeScore+' : '+matchDetails.awayScore+' <span class="team_name left_right_margin">'+matchDetails.homeTeam+'</span></div> <div id="field_area"></div>';
+}
+
 
 
 export const sidebar = () => {
-  return '<div id="sidebar"><div id="header"><img src="../src/assets/img/sideline.png" height="70"/></div><div id="logo">SIDELINE \'18</div>'+loginDom+'</div><div id="playground"></div>';
+  return '<div id="sidebar"><div id="header"><img src="../src/assets/img/sideline.png" height="70"/></div><div id="logo">SIDELINE \'18</div>'+loginDom+userScore+'</div><div id="playground"></div>';
 };
 
 export const playground = () => {
